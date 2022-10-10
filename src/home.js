b41980f6f0d5aadb5786faa987b23f3aaf3189b4
Rahat Islam Akash <ami@pop-os.localdomain>
2022-10-11 03:21:00 +0600

    click event in react

--- src/home.js
+++ src/home.js
@@ -1,11 +1,17 @@
 const Home = () => {
-    const handleClick = ()=> {
-        console.log('tarpor bolo');
+    const handleClick = (e)=> {
+        console.log('tarpor bolo', e);
+    }
+
+    const handleAgain = (name) => {
+        console.log('hello'+name);
     }
     return ( 
         <div className="home">
             <h1>Home </h1>
+            {/* click event in react */}
             <button onClick={handleClick()}>click me</button>
+            <button onClick={()=> handleAgain('Akash')}>click again</button>
         </div>
      );
 }
