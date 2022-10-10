6bc020a448fb8eea49792538e7bb619e332e63da
Rahat Islam Akash <ami@pop-os.localdomain>
2022-10-10 19:08:39 +0600

    first project with react js

---
+++ src/nevbar.js
@@ -0,0 +1,16 @@
+const Navebar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>Logo</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact Us</a>
+                
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default Navebar;
