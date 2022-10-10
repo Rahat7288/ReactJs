2a5c2106785f9b4f5272d1ea9bcb0d443f164db2
Rahat Islam Akash <ami@pop-os.localdomain>
2022-10-11 03:56:39 +0600

    list item in react

--- src/home.js
+++ src/home.js
@@ -1,21 +1,23 @@
 import { useState } from "react";
 const Home = () => {
-    const [name, setName] = useState('user name')
-    const handleClick = ()=> {
-        setName('akash')
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'my new website', body: 'web body', author: 'ami', id: 1},
+        {title: 'my new website2', body: 'web body2', author: 'ami', id: 2},
+        {title: 'my new website3', body: 'web body3', author: 'ami', id: 3},
 
-    // const handleAgain = (name) => {
-    //     console.log('hello'+name);
-    // }
+    ])
     return ( 
         <div className="home">
-            <h1>Home </h1>
+            {blogs.map((blog) => (
+                // we need different key for verify for list item
+                <div className="blog-priview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <h2>{blog.body}</h2>
+                    <h3>{blog.author}</h3>
+
+                </div>
+            ))}
             
-            <p>{name}</p>
-            {/* click event in react */}
-            <button onClick={handleClick}>click me</button>
-            {/* <button onClick={()=> handleAgain('Akash')}>click again</button> */}
         </div>
      );
 }
